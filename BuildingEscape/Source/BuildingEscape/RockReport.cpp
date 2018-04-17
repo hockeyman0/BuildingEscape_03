424fca138d5f9dff356d835abf9495da6161b2f8
Chris Holly <christopher.holly@outlook.com>
2018-04-16 21:57:37 -0700

    Now with Actor Name

--- BuildingEscape/Source/BuildingEscape/RockReport.cpp
+++ BuildingEscape/Source/BuildingEscape/RockReport.cpp
@@ -1,34 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
-#include "GameFramework/Actor.h"
+#include "RockReport.h"
+
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+URockReport::URockReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-
-    
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void URockReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-    FString ObjectName = GetOwner()->GetName();
-
-    UE_LOG(LogTemp, Warning, TEXT("Position report for %s"), *ObjectName);
+    UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Rocker!"));
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void URockReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
